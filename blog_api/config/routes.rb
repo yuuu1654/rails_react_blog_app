f38e05707ac936b7e4ccc7a00016bd84482d0876
森下勇樹 <>
2025-02-02 14:04:25 +0900

    rails g controller api::v1::posts index show create update destroy

--- blog_api/config/routes.rb
+++ blog_api/config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      get "posts/index"
+      get "posts/show"
+      get "posts/create"
+      get "posts/update"
+      get "posts/destroy"
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
